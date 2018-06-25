7ddc2f1efee8242d0021c3fabbdb27eba27e4bde
HeZaichang.WeRDyin <heyin223@163.com>
2018-06-25 22:02:14 +0800

    Unit test: button component

--- test/unit/Btn.spec.js
+++ test/unit/Btn.spec.js
@@ -2,13 +2,63 @@ import Button from '@/button'
 import { shallowMount } from '@vue/test-utils'
 
 describe('Button', () => {
-	it('render Button', () => {
-		const wrapper = shallowMount(Button, {
+	it('render', () => {
+		const wrapper = shallowMount(Button)
+		expect(wrapper.isVueInstance()).toBeTruthy()
+		expect(wrapper.classes()).toContain('o-Btn--default')
+	})
+
+	it('disabled', () => {
+		const wrapper = shallowMount(Button,{
 			propsData: {
 				disabled: true
 			}
 		})
+		expect(wrapper.classes()).toContain('is-disabled')
+	})
 
-		expect(wrapper.isVueInstance()).toBeTruthy()
+	it('readonly', () => {
+		const wrapper = shallowMount(Button,{
+			propsData: {
+				loading: true
+			}
+		})
+		expect(wrapper.classes()).toContain('is-loading')
+	})
+
+	it('round', () => {
+		const wrapper = shallowMount(Button,{
+			propsData: {
+				round: true
+			}
+		})
+		expect(wrapper.classes()).toContain('o-Btn--round')
+	})
+
+	it('block', () => {
+		const wrapper = shallowMount(Button,{
+			propsData: {
+				block: true
+			}
+		})
+		expect(wrapper.classes()).toContain('o-Btn--block')
+	})
+
+	it('gradient', () => {
+		const wrapper = shallowMount(Button,{
+			propsData: {
+				gradient: true
+			}
+		})
+		expect(wrapper.classes()).toContain('o-Btn--gradient')
+	})
+
+	it('ghost', () => {
+		const wrapper = shallowMount(Button,{
+			propsData: {
+				ghost: true
+			}
+		})
+		expect(wrapper.classes()).toContain('o-Btn--ghost')
 	})
 })
